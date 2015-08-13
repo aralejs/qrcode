469beae69ec4e54a9dc2083e473e33935ee8048f
sehuo <stu.cbd@gmail.com>
2015-08-13 10:45:26 +0800

    删除对jquery的依赖

--- src/qrcode.js
+++ src/qrcode.js
@@ -1,4 +1,3 @@
-var $ = require('jquery');
 var assign = require('object-assign');
 var QRCodeAlg = require('./qrcodealg');
 var qrcodeAlgObjCache = [];
@@ -45,78 +44,78 @@ var inPositionDetection = function(row, col, count){
  * 二维码构造函数，主要用于绘制
  * @param  {参数列表} opt 传递参数
  * @return {}
- */	
+ */    
 var qrcode = function(opt) {
-	if (typeof opt === 'string') { // 只编码ASCII字符串
-		opt = {
-			text: opt
-		};
-	}
-	//设置默认参数
-	this.options = assign({}, {
-		text:"",
-		render: "",
-		width: 256,
-		height: 256,
-		correctLevel: 3,
-		background: "#ffffff",
-		foreground: "#000000",
+    if (typeof opt === 'string') { // 只编码ASCII字符串
+        opt = {
+            text: opt
+        };
+    }
+    //设置默认参数
+    this.options = assign({}, {
+        text:"",
+        render: "",
+        width: 256,
+        height: 256,
+        correctLevel: 3,
+        background: "#ffffff",
+        foreground: "#000000",
         image : '',
         imageWidth: 30,
         imageHeight: 30
-	}, opt);
+    }, opt);
 
-	//使用QRCodeAlg创建二维码结构
-	var qrCodeAlg = null;
-	for(var i = 0, l = qrcodeAlgObjCache.length; i < l; i++){
-		if(qrcodeAlgObjCache[i].text == this.options.text && qrcodeAlgObjCache[i].text.correctLevel == this.options.correctLevel){
-			qrCodeAlg = qrcodeAlgObjCache[i].obj;
-			break;
-		}
-	}
+    //使用QRCodeAlg创建二维码结构
+    var qrCodeAlg = null;
+    for(var i = 0, l = qrcodeAlgObjCache.length; i < l; i++){
+        if(qrcodeAlgObjCache[i].text == this.options.text && qrcodeAlgObjCache[i].text.correctLevel == this.options.correctLevel){
+            qrCodeAlg = qrcodeAlgObjCache[i].obj;
+            break;
+        }
+    }
 
-	if(i == l){
-	  qrCodeAlg = new QRCodeAlg(this.options.text, this.options.correctLevel);
-	  qrcodeAlgObjCache.push({text:this.options.text, correctLevel: this.options.correctLevel, obj:qrCodeAlg});
-	}
+    if(i == l){
+      qrCodeAlg = new QRCodeAlg(this.options.text, this.options.correctLevel);
+      qrcodeAlgObjCache.push({text:this.options.text, correctLevel: this.options.correctLevel, obj:qrCodeAlg});
+    }
 
-	if(this.options.render){
-		switch (this.options.render){
-			case "canvas":
-				return this.createCanvas(qrCodeAlg);
-			case "table":
-				return this.createTable(qrCodeAlg);
-			case "svg":
-				return this.createSVG(qrCodeAlg);
-			default:
-				return this.createDefault(qrCodeAlg);
-		}
-	}
-	return this.createDefault(qrCodeAlg);
+    if(this.options.render){
+        switch (this.options.render){
+            case "canvas":
+                return this.createCanvas(qrCodeAlg);
+            case "table":
+                return this.createTable(qrCodeAlg);
+            case "svg":
+                return this.createSVG(qrCodeAlg);
+            default:
+                return this.createDefault(qrCodeAlg);
+        }
+    }
+    return this.createDefault(qrCodeAlg);
 };
 
 assign(qrcode.prototype,{
 
     createDefault (qrCodeAlg) {
-    	var canvas = document.createElement('canvas');
-    	if(canvas.getContext)
-    		return this.createCanvas(qrCodeAlg);
-    	SVG_NS = 'http://www.w3.org/2000/svg';
-      	if( !!document.createElementNS && !!document.createElementNS(SVG_NS, 'svg').createSVGRect )
-      		return this.createSVG(qrCodeAlg);
-    	return this.createTable(qrCodeAlg);
+        var canvas = document.createElement('canvas');
+        if(canvas.getContext)
+            return this.createCanvas(qrCodeAlg);
+        SVG_NS = 'http://www.w3.org/2000/svg';
+          if( !!document.createElementNS && !!document.createElementNS(SVG_NS, 'svg').createSVGRect )
+              return this.createSVG(qrCodeAlg);
+        return this.createTable(qrCodeAlg);
     },
     /**
      * 使用Canvas来画二维码
      * @return {}
      */
     createCanvas (qrCodeAlg) {
-    	//创建canvas节点
+        //创建canvas节点
         var that = this;
-    	var canvas = document.createElement('canvas');
-    	canvas.width = that.options.width;
-    	canvas.height = that.options.height;
-    	var ctx = canvas.getContext('2d');
+        var canvas = document.createElement('canvas');
+        canvas.width = that.options.width;
+        canvas.height = that.options.height;
+        var ctx = canvas.getContext('2d');
         var count = qrCodeAlg.getModuleCount();
         // 预载图片
         var loadImage = function(url,callback){
@@ -127,13 +126,13 @@ assign(qrcode.prototype,{
                 img.onload = null
             };
         }
-    	//计算每个点的长宽
-    	var tileW = (that.options.width / count).toPrecision(4);
-    	var tileH = (that.options.height / count).toPrecision(4);
+        //计算每个点的长宽
+        var tileW = (that.options.width / count).toPrecision(4);
+        var tileH = (that.options.height / count).toPrecision(4);
 
         //绘制
-    	for (var row = 0; row < count; row++) {
-    		for (var col = 0; col < count; col++) {
+        for (var row = 0; row < count; row++) {
+            for (var col = 0; col < count; col++) {
                 var w = (Math.ceil((col + 1) * tileW) - Math.floor(col * tileW));
                 var h = (Math.ceil((row + 1) * tileW) - Math.floor(row * tileW));
                 var foreground = getForeGround({
@@ -144,8 +143,8 @@ assign(qrcode.prototype,{
                 });
                 ctx.fillStyle = qrCodeAlg.modules[row][col] ? foreground : that.options.background;
                 ctx.fillRect(Math.round(col * tileW), Math.round(row * tileH), w, h);
-    		}
-    	}
+            }
+        }
         if(that.options.image){
             loadImage(that.options.image, function(img){
                 var x = ((that.options.width - that.options.imageWidth)/2).toFixed(2);
@@ -153,25 +152,25 @@ assign(qrcode.prototype,{
                 ctx.drawImage(img, x, y, that.options.imageWidth, that.options.imageHeight);
             });
         }
-    	//返回绘制的节点
-    	return canvas;
+        //返回绘制的节点
+        return canvas;
     },
     /**
      * 使用table来绘制二维码
      * @return {}
      */
     createTable (qrCodeAlg) {
-    	var count = qrCodeAlg.getModuleCount();
+        var count = qrCodeAlg.getModuleCount();
        
-    	// 计算每个节点的长宽；取整，防止点之间出现分离
-    	var tileW = Math.floor(this.options.width / count);
-    	var tileH = Math.floor(this.options.height / count);
-    	if(tileW <= 0){
-    		tileW = count < 80 ? 2 : 1;
-    	}
-    	if(tileH <= 0){
-    		tileH = count < 80 ? 2 : 1;
-    	}
+        // 计算每个节点的长宽；取整，防止点之间出现分离
+        var tileW = Math.floor(this.options.width / count);
+        var tileH = Math.floor(this.options.height / count);
+        if(tileW <= 0){
+            tileW = count < 80 ? 2 : 1;
+        }
+        if(tileH <= 0){
+            tileH = count < 80 ? 2 : 1;
+        }
 
         //创建table节点
         //重算码大小
@@ -180,23 +179,23 @@ assign(qrcode.prototype,{
             this.options.background +
             ';">');
 
-    	// 绘制二维码
-    	var backTd = '<td style="border:0px; margin:0px; padding:0px; width:'+tileW+'px; background-color: '+this.options.background+'"></td>';
+        // 绘制二维码
+        var backTd = '<td style="border:0px; margin:0px; padding:0px; width:'+tileW+'px; background-color: '+this.options.background+'"></td>';
 
-    	for (var row = 0; row < count; row++) {
-    		s.push('<tr style="border:0px; margin:0px; padding:0px; height: ' + tileH +'px">');
-    		for (var col = 0; col < count; col++) {
+        for (var row = 0; row < count; row++) {
+            s.push('<tr style="border:0px; margin:0px; padding:0px; height: ' + tileH +'px">');
+            for (var col = 0; col < count; col++) {
                 var foreground = getForeGround({
                     row : row,
                     col : col,
                     count : count,
                     options : this.options
                 });
-    			s.push(qrCodeAlg.modules[row][col] ? '<td style="border:0px; margin:0px; padding:0px; width:'+tileW+'px; background-color: '+ foreground+'"></td>' : backTd);
-    		}
-    		s.push('</tr>');
-    	}
-    	s.push('</table>');
+                s.push(qrCodeAlg.modules[row][col] ? '<td style="border:0px; margin:0px; padding:0px; width:'+tileW+'px; background-color: '+ foreground+'"></td>' : backTd);
+            }
+            s.push('</tr>');
+        }
+        s.push('</table>');
 
         if(this.options.image){
             // 计算表格的总大小
@@ -214,62 +213,64 @@ assign(qrcode.prototype,{
             s.push('</div>');
         }
 
-    	var span = document.createElement("span");
+        var span = document.createElement("span");
         span.innerHTML=s.join('');
 
-    	return span.firstChild;
+        return span.firstChild;
     },
     /**
      * 使用SVG开绘制二维码
      * @return {}
      */
     createSVG (qrCodeAlg){
-        var x, dx, y, dy,
-            count = qrCodeAlg.getModuleCount(),
-        	scale = this.options.height / this.options.width,
-        	svg = '<svg xmlns="http://www.w3.org/2000/svg" '
-          	    + 'width="'+ this.options.width + 'px" height="' + this.options.height + 'px" '
-                + 'viewbox="0 0 ' + count * 10 + ' ' + count * 10 * scale + '">',
-            rectHead = '<path ',
-            backRect = ' style="stroke-width:0.5;stroke:' + this.options.background
-                + ';fill:' + this.options.background + ';"></path>';
+        let count = qrCodeAlg.getModuleCount();
+        let scale = this.options.height / this.options.width;
 
-        // draw in the svg
-        for (var row = 0; row < count; row++) {
-            for (var col = 0; col < count; col++) {
-                var foreground = getForeGround({
+        // create svg
+        let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svg.setAttribute("width", this.options.width);
+        svg.setAttribute("height", this.options.height);
+        svg.setAttribute("viewbox", `0 0 ${count * 10} ${count * 10 * scale}`);
+
+        for (let row = 0; row < count; row++) {
+            for (let col = 0; col < count; col++) {
+                let x = col * 10;
+                let y = row * 10 * scale;
+                let dx = (col + 1) * 10;
+                let dy = (row + 1) * 10 * scale;
+                let foreground = getForeGround({
                     row : row,
                     col : col,
                     count : count,
                     options : this.options
                 });
-                var foreRect = ' style="stroke-width:0.5;stroke:' + foreground 
-                    + ';fill:' + foreground + ';"></path>';
-                x = col * 10;
-                y = row * 10 * scale;
-                dx = (col + 1) * 10;
-                dy = (row + 1) * 10 * scale;
-                
-                svg += rectHead + 'd="M ' + x + ',' + y
-                    + ' L ' + dx + ',' + y
-                    + ' L ' + dx + ',' + dy
-                    + ' L ' + x + ',' + dy
-                    + ' Z"';
-                    
-                svg += qrCodeAlg.modules[row][ col] ? foreRect : backRect;
+                // create path
+                let path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+                path.setAttribute('d', `M ${x},${y} L ${dx},${y} L ${dx},${dy} L ${x},${dy} Z`);
+                path.setAttribute('stroke-width', '0.5');
+
+                if(qrCodeAlg.modules[row][ col]){
+                    path.setAttribute('fill', foreground);
+                    path.setAttribute('stroke', foreground);
+                }else{
+                    path.setAttribute('fill', this.options.background);
+                    path.setAttribute('stroke', this.options.background);
+                }
+                svg.appendChild(path);
             }
         }
-
+        // create image
         if(this.options.image){
-            var x = ((this.options.width - this.options.imageWidth)/2).toFixed(2);
-            var y = ((this.options.height - this.options.imageHeight)/2).toFixed(2);
-            svg += '<image xlink:href="'+ this.options.image + '" x="'+ x +'" y="'+ y +'" width="'+ this.options.imageWidth +'" height="'+ this.options.imageHeight +'"/>';
+            let img = document.createElementNS('http://www.w3.org/2000/svg', 'image');
+            img.setAttributeNS("http://www.w3.org/1999/xlink","href",this.options.image);
+            img.setAttribute('x', ((this.options.width - this.options.imageWidth)/2).toFixed(2));
+            img.setAttribute('y', ((this.options.height - this.options.imageHeight)/2).toFixed(2));
+            img.setAttribute('width', this.options.imageWidth);
+            img.setAttribute('height', this.options.imageHeight);
+            svg.appendChild(img);
         }
 
-        svg += '</svg>';
-
-        // return just built svg
-        return $(svg)[0];
+        return svg;
     }
 });
 module.exports = qrcode;
